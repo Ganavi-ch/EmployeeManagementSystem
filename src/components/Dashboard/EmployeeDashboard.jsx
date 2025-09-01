2e93ef03bd7b29007632a2d9735b7b4a3c79a370
Ganavi-ch <chganavi70@gmail.com>
2025-09-01 10:54:39 +0530

    fixed EmployeeDashboard bug

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
+import Header from '../other/Header'
+import TaskListNumbers from '../other/TaskListNumbers'
+import TaskList from '../TaskList/TaskList'
 
-const AcceptTask = ({data}) => {
-    console.log();
+const EmployeeDashboard = (props) => {
+
   return (
-    <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
-            <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
-                <h4 className='text-sm'>{data.taskDate}</h4>
-            </div>
-            <h2 className='mt-5 text-2xl font-semibold'>{data.taskTitle}</h2>
-            <p className='text-sm mt-2'>
-                {data.taskDescription}
-            </p>
-            <div className='flex justify-between mt-6 '>
-                <button className='bg-green-500 rounded font-medium py-1 px-2 text-xs'>Mark as Completed</button>
-                <button className='bg-red-500 rounded font-medium py-1 px-2 text-xs'>Mark as Failed</button>
-            </div>
-        </div>
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
+        
+        <Header changeUser={props.changeUser} data={props.data}/>
+        <TaskListNumbers data={props.data} />
+        <TaskList data={props.data} />
+    </div>
   )
 }
 
-export default AcceptTask
+export default EmployeeDashboard
